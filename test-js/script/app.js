df58b21fbdd692661368b6a96ef674699b79046d
GrafVishna <94762427+GrafVishna@users.noreply.github.com>
2022-12-05 13:42:36 +0200

    Add files via upload

--- test-js/script/app.js
+++ test-js/script/app.js
@@ -6,15 +6,17 @@ document.addEventListener("click", buttonClick);
 function buttonClick(event) {
    const targetItem = event.target;
    console.log(targetItem);
-   if (targetItem.closest(".test__button")) buttons.forEach(((button, index) => {
-      let buttonActive = event.target;
-      buttonsLine.style.width = `calc(100% / ${index + 1})`;
-      if (button == buttonActive) {
-         button.classList.add("active");
-         buttonsLine.classList.add("active");
-         buttonsLine.style.transform = `translateX(calc(${index} * 100%))`;
-         buttonsLine.style.opacity = "1";
-         console.log(index);
-      } else button.classList.remove("active");
-   }));
+   if (targetItem.closest(".test__button")) {
+      buttons.forEach(((button, index) => {
+         let buttonActive = event.target;
+         buttonsLine.style.width = `calc(100% / ${index + 1})`;
+         if (button == buttonActive.closest('.test__button')) {
+            button.classList.add("active");
+            buttonsLine.classList.add("active");
+            buttonsLine.style.transform = `translateX(calc(${index} * 100%))`;
+            buttonsLine.style.opacity = "1";
+            console.log(index);
+         } else button.classList.remove("active");
+      }))
+   }
 }
