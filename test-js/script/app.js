78386c4038ee91f3ace308e952d0f1f35705cc2d
GrafVishna <94762427+GrafVishna@users.noreply.github.com>
2022-12-05 18:10:52 +0200

    Add files via upload

--- test-js/script/app.js
+++ test-js/script/app.js
@@ -1,21 +1,27 @@
-const buttonsWrapper = document.querySelector(".test__buttons");
-const buttons = document.querySelectorAll(".test__button");
-const buttonsLine = document.createElement("span");
-buttonsWrapper.prepend(buttonsLine);
-document.addEventListener("click", buttonClick);
-function buttonClick(event) {
-   const targetItem = event.target;
-   console.log(targetItem);
-   if (targetItem.closest(".test__button")) {
-      buttons.forEach(((button, index) => {
-         buttonsLine.style.width = `calc(100% / ${index + 1})`;
-         if (button == targetItem.closest('.test__button')) {
-            button.classList.add("active");
-            buttonsLine.classList.add("active");
-            buttonsLine.style.transform = `translateX(calc(${index} * 100%))`;
-            buttonsLine.style.opacity = "1";
-            console.log(index);
-         } else button.classList.remove("active");
-      }))
+
+function buttonsLineHover(buttonsWrapper) {
+   const buttons = buttonsWrapper.querySelectorAll("button");
+   const buttonsLine = document.createElement("span");
+   buttonsWrapper.prepend(buttonsLine);
+   document.addEventListener("click", buttonClick);
+   function buttonClick(event) {
+      const targetItem = event.target;
+      console.log(targetItem);
+      if (targetItem.closest("button")) {
+         buttons.forEach(((button, index) => {
+            buttonsLine.style.width = `calc(100% / ${index + 1})`;
+            if (button == targetItem.closest('button')) {
+               button.classList.add("active");
+               buttonsLine.classList.add("active");
+               buttonsLine.style.transform = `translateX(calc(${index} * 100%))`;
+               buttonsLine.style.opacity = "1";
+               console.log(index);
+            } else {
+               button.classList.remove("active")
+            }
+         }))
+      }
    }
 }
+
+buttonsLineHover(document.querySelector("[data-buttons-anim]"))
