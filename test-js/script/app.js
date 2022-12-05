052fa1af0172600a2f47c233879b08c3485baaef
GrafVishna <94762427+GrafVishna@users.noreply.github.com>
2022-12-05 12:22:46 +0200

    Add files via upload

---
+++ test-js/script/app.js
@@ -0,0 +1,20 @@
+const buttonsWrapper = document.querySelector(".test__buttons");
+const buttons = document.querySelectorAll(".test__button");
+const buttonsLine = document.createElement("span");
+buttonsWrapper.prepend(buttonsLine);
+document.addEventListener("click", buttonClick);
+function buttonClick(event) {
+   const targetItem = event.target;
+   console.log(targetItem);
+   if (targetItem.closest(".test__button")) buttons.forEach(((button, index) => {
+      let buttonActive = event.target;
+      buttonsLine.style.width = `calc(100% / ${index + 1})`;
+      if (button == buttonActive) {
+         button.classList.add("active");
+         buttonsLine.classList.add("active");
+         buttonsLine.style.transform = `translateX(calc(${index} * 100%))`;
+         buttonsLine.style.opacity = "1";
+         console.log(index);
+      } else button.classList.remove("active");
+   }));
+}
